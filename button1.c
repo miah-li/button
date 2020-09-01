a4770c166df24eb94b56d1d823b87ea3f9f4e3b6
miah <miah.li@wesion.com>
2020-09-01 16:57:20 +0800

    add button1.c
    Signed-off-by: miah <miah.li@wesion.com>

---
+++ button1.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <linux/input.h>
+struct event {
+	struct timeval time;
+	unsigned short type;
+	unsigned short code;
+	unsigned int value;
+
+};
+int main()
+{
+	
+	int button_fd,led_fd;
+	char *buf1 = "default-on", *buf2 = "none";
+	struct input_event event;
+	button_fd = open("/dev/input/event3", O_RDWR);
+	if(button_fd<0)
+	{
+		printf("open error!\n");
+	}
+	led_fd = open("/sys/class/leds/red_led/trigger",O_RDWR);
+	if(led_fd < 0)
+        {
+                printf("Open led device faild!\n");
+        }
+	while(1)
+	{	
+		read(button_fd, &event, sizeof(event));
+		if(event.type == EV_KEY)
+               {
+			printf("%d\n",event.value);
+			if(event.value == 1)
+			{
+				write(led_fd,buf1,strlen(buf1));
+			}
+			else
+			{
+				write(led_fd,buf2,strlen(buf2));
+			}
+             }
+        }
+        close(button_fd);
+        close(led_fd);
+        return 0;
+}
+
